ee6ee25cf4c8d8daa06292f071f6717c0a186197
Carol Santos <anacssnts@gmail.com>
2021-05-22 23:30:14 -0300

    wip do carrossel

--- assets/js/carrousel.js
+++ assets/js/carrousel.js
@@ -2,30 +2,42 @@
 	const carrousel = document.querySelector('.carrousel');
 	const trampos = document.querySelectorAll('.trampo');
 
-	for (var i = 0; i = trampos.length; i = i + 1) {
+	console.log(trampos);
+
+
+
+
+	let i = 0;
+	while (i < trampos.length) {
 		(function(i) {
-			setInterval(function() {
-				console.log(i + "=>" + list[i] + "\n");
-			}, 100)
-		})(i);
+			setTimeout(function() {
+				if (i === 0) {
+					console.log(i)
+					trampos[0].className = 'trampos --active';
+					trampos[1].className = 'trampos --next';
+					trampos[2].className = 'trampos';
+				}
+				else if (i === 1) {
+					console.log(i)
+					trampos[0].className = 'trampos';
+					trampos[1].className = 'trampos --active';
+					trampos[2].className = 'trampos --next';
+				}
+				else if (i === 2) {
+					console.log(i)
+					trampos[0].className = 'trampos --next';
+					trampos[1].className = 'trampos';
+					trampos[2].className = 'trampos --active';
+				}
+			}, 1000 * i);
+		})(i++)
+		if (i === 2) {
+			console.log("reseta filho da puta");
+			i = 0;
+		}
 	}
 
-	for() {
-		if (i === 0) {
-			trampos[0].className = 'trampos --active';
-			trampos[1].className = 'trampos --next';
-			trampos[2].className = 'trampos';
-		}
-		else if (i === 1) {
-			trampos[0].className = 'trampos';
-			trampos[1].className = 'trampos --active';
-			trampos[2].className = 'trampos --next';
-		}
-		else if (i === 2) {
-			trampos[0].className = 'trampos --next';
-			trampos[1].className = 'trampos';
-			trampos[2].className = 'trampos --active';
-	}
+	
 
 
 })();
